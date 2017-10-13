81225f3643735ae99a1f9151ba4c281a864a9e1e
ALltairCA <alexvista1234@gmail.com>
2017-10-13 11:45:59 +0530

    addd roles

---
+++ src/java/Models/Role.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Models;
+
+/**
+ *
+ * @author Altair
+ */
+public class Role {
+    private int roleID;
+    private String title;
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
