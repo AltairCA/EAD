4f2e663efee413a2a7151efa4efae0b71c5d018a
ALltairCA <alexvista1234@gmail.com>
2017-10-14 15:20:19 +0530

    added db script

---
+++ DBScript.sql
@@ -0,0 +1,17 @@
+create table TASK
+(
+	TASKID INTEGER not null primary key,
+	DESCRIPTION VARCHAR(255),
+	EMPLOYEEID INTEGER
+);
+create table ROLE
+(
+	ROLEID INTEGER not null primary key,
+	TITLE VARCHAR(100)
+);
+create table "APP".EMPLOYEE
+(
+	EMPLOYEEID INTEGER not null primary key,
+	NAME VARCHAR(100),
+	ROLEID INTEGER
+);
